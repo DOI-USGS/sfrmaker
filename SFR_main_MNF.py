630b19eff18091e93c2a196e6ef89820d06253d4
Mike Fienen <mnfienen@usgs.gov>
2014-02-18 17:19:48 -0600

    Revised SFR_plots.py to only plot DEM and/or Contours if their respective intersect routines were called.

--- SFR_main_MNF.py
+++ SFR_main_MNF.py
@@ -3,21 +3,19 @@ __author__ = 'Fienen, Reeves, Leaf - USGS'
 import SFR_classes as SFRc
 import sfr_plots
 
-infile = 'SFR_input.xml'
+infile = 'SFR_input_NACP.xml'
 
 SFRdata = SFRc.SFRInput(infile)
 
 SFRpre = SFRc.SFRpreproc(SFRdata)
 
-if SFRdata.preproc:
-    print 'Running preprocessing routine'
-
-    SFRpre.clip_and_join_attributes()
-
 SFRops = SFRc.SFROperations(SFRdata)
 
-#SFRops.assign_layers(SFRdata)
+if SFRdata.preproc:
+    print 'Running preprocessing routine'
+    SFRpre.clip_and_join_attributes(SFRops)
 
+print 'runnning intersect'
 SFRops.intersect()
 
 FragIDdata = SFRc.FragIDPropsAll()
@@ -48,12 +46,12 @@ LevelPathdata.return_cutoffs(FragIDdata, CELLdata, SFRdata)
 
 
 
-
+'''
 saveme ={'SFRdata':  SFRdata, 'COMIDdata': COMIDdata, 'FragIDdata': FragIDdata,
               'SFRops': SFRops, 'LevelPathdata' : LevelPathdata, 'CELLdata' : CELLdata}
 
 SFRc.savetmp(saveme)
-'''
+
 a = SFRc.loadtmp(['SFRdata', 'CELLdata', 'COMIDdata', 'FragIDdata', 'SFRops', 'LevelPathdata'])
 
 SFRdata = a['SFRdata']
@@ -61,54 +59,62 @@ COMIDdata = a['COMIDdata']
 FragIDdata = a['FragIDdata']
 SFRops = a['SFRops']
 LevelPathdata = a['LevelPathdata']
+CELLdata = a['CELLdata']
 
+# Get streambed elevation information from topographic contours
+SFRpre.intersect_contours(SFRdata) # this needs to be in the example Main!
+ContourElevs = SFRc.ElevsFromContours(SFRdata)
+ContourElevs.get_contour_intersections(FragIDdata, COMIDdata)
+ContourElevs.assign_elevations_to_FragID(FragIDdata, COMIDdata)
+'''
+# Get streambed elevation information from DEM
+SFRpre.intersect_DEM(SFRdata)  # this needs to be in the example Main!
 DEMelevs = SFRc.ElevsFromDEM()
 DEMelevs.DEM_elevs_by_FragID(SFRdata, SFRops)
-
 DEMelevs.connect_downhill(FragIDdata)
 
-SFRp = sfr_plots.plot_segments(SFRdata, COMIDdata)
+# Comparison plots of streambed elevations (by COMID) for different elevation methods
+SFRp = sfr_plots.plot_elevation_profiles(SFRdata)
 SFRp.read_DIS()
-SFRp.get_segment_plotting_info(FragIDdata)
-SFRp.plot_profiles('elevs_from_contours.pdf')
-
+SFRp.get_comid_plotting_info(FragIDdata, COMIDdata, SFRdata)
+SFRp.plot_profiles('Elevation_method_comparison.pdf')
+'''
 saveme ={'COMIDdata' : COMIDdata,
          'FragIDdata' : FragIDdata,
          'LevelPathdata' : LevelPathdata,
-         'CELLdata' : CELLdata}
+         'CELLdata' : CELLdata, 'SFRdata':  SFRdata,}
 SFRc.savetmp(saveme)
 
+
+a = SFRc.loadtmp['SFRdata', 'COMIDdata', 'CELLdata', 'FragIDdata', 'LevelPathdata']
+
+COMIDdata = a['COMIDdata']
+FragIDdata = a['FragIDdata']
+LevelPathdata = a['LevelPathdata']
+CELLdata = a['CELLdata']
+SFRdata = a['SFRdata']
 '''
-loadme = ['COMIDdata', 'CELLdata', 'FragIDdata', 'LevelPathdata']
+SFRops.reach_ordering(COMIDdata,
+                      FragIDdata,
+                      LevelPathdata)
 
-instuff = SFRc.loadtmp(loadme)
-
-
-
-
-
-
-
-
-
-SFRops.reach_ordering(instuff['COMIDdata'],
-                      instuff['FragIDdata'],
-                      instuff['LevelPathdata'])
-SFRpre.intersect_contours(SFRdata)
 Segmentdata = SFRc.SFRSegmentsAll()
-ContourElevs = SFRc.ElevsFromContours(SFRdata)
-ContourElevs.get_contour_intersections(instuff['FragIDdata'], instuff['COMIDdata'])
-Segmentdata.divide_at_confluences(instuff['LevelPathdata'], instuff['FragIDdata'],
-                                  instuff['COMIDdata'], instuff['CELLdata'], SFRdata)
-Segmentdata.accumulate_same_levelpathID(instuff['LevelPathdata'], instuff['COMIDdata'],
-                                        instuff['FragIDdata'], SFRdata, instuff['CELLdata'])
+
+Segmentdata.divide_at_confluences(LevelPathdata, FragIDdata,
+                                  COMIDdata, CELLdata, SFRdata)
+Segmentdata.accumulate_same_levelpathID(LevelPathdata, COMIDdata,
+                                        FragIDdata, SFRdata, CELLdata)
+
+# plot SFR segment profiles
+SFRp.get_segment_plotting_info(Segmentdata)
+SFRp.plot_profiles('Segment_profiles.pdf')
 
 #make some output
-
 SFRoutput = SFRc.SFRoutput(SFRdata)
 SFRoutput.write_SFR_tables(Segmentdata)
+SFRops.assign_layers(SFRdata)
 SFRoutput.build_SFR_package()
-SFRoutput.build_SFR_shapefile(Segmentdata)
-
+#SFRoutput.build_SFR_shapefile(Segmentdata)
+print "Done"
 
 i = 2
