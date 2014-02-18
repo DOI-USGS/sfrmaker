5e2c656da448a51e915dea0d6c590cfac52d1f97
Mike Fienen <mnfienen@usgs.gov>
2014-02-18 11:39:50 -0600

    premerge - moved my folder so no real changes

--- SFR_main_HWR.py
+++ SFR_main_HWR.py
@@ -8,10 +8,12 @@ infile = 'SFR_input.xml'
 SFRdata = SFRc.SFRInput(infile)
 
 SFRops = SFRc.SFROperations(SFRdata)
-'''
+
+SFRpre = SFRc.SFRpreproc(SFRdata)
+
+
 if SFRdata.preproc:
     print 'Running preprocessing routine'
-    SFRpre = SFRc.SFRpreproc(SFRdata)
 
     SFRpre.clip_and_join_attributes(SFRops)
 
@@ -31,11 +33,11 @@ SFRops.make_rivers_table(FragIDdata)
 FragIDdata.populate_elevations(SFRdata)
 
 COMIDdata = SFRc.COMIDPropsAll()
-'''
+
 CELLdata = SFRc.CellPropsAll()
 
 CELLdata.populate_cells(SFRdata)
-'''
+
 LevelPathdata = SFRc.LevelPathIDpropsAll()
 
 COMIDdata.populate_routing(SFRdata, FragIDdata, LevelPathdata, CELLdata)
@@ -47,33 +49,28 @@ FragIDdata.return_cellnum_LevelPathID(LevelPathdata)
 LevelPathdata.return_cutoffs(FragIDdata, CELLdata, SFRdata)
 
 SFRops.reach_ordering(COMIDdata, FragIDdata, LevelPathdata)
-
-SFRpre.intersect_contours(SFRdata)
+'''
+SFRpre.intersect_contours(SFRdata) # this needs to be in the example Main!
 ContourElevs = SFRc.ElevsFromContours(SFRdata)
 ContourElevs.get_contour_intersections(FragIDdata, COMIDdata)
-
-SFRpre.intersect_DEM(SFRdata)
+ContourElevs.assign_elevations_to_FragID(FragIDdata, COMIDdata)
+'''
+SFRpre.intersect_DEM(SFRdata) # this needs to be in the example Main!
 DEMelevs = SFRc.ElevsFromDEM()
 DEMelevs.DEM_elevs_by_FragID(SFRdata, SFRops)
-
-
-DEMelevs = SFRc.ElevsFromDEM()
-DEMelevs.DEM_elevs_by_FragID(SFRdata, SFRops)
-
-Contour_elevs.assign_elevations_to_FragID(FragIDdata, COMIDdata)
 DEMelevs.connect_downhill(FragIDdata)
 
-SFRp = sfr_plots.plot_segments(SFRdata, COMIDdata)
+SFRp = sfr_plots.plot_elevation_profiles(SFRdata, COMIDdata)
 SFRp.read_DIS()
-SFRp.get_segment_plotting_info(FragIDdata)
+SFRp.get_comid_plotting_info(FragIDdata)
 SFRp.plot_profiles('elevs_from_contours.pdf')
-
+'''
 saveme ={'COMIDdata' : COMIDdata,
          'FragIDdata' : FragIDdata,
          'LevelPathdata' : LevelPathdata,
          'CELLdata' : CELLdata}
 SFRc.savetmp(saveme)
-'''
+
 
 loadme = ['COMIDdata', 'FragIDdata', 'LevelPathdata']
 instuff = SFRc.loadtmp(loadme)
@@ -81,6 +78,9 @@ instuff = SFRc.loadtmp(loadme)
 LevelPathdata = instuff['LevelPathdata']
 FragIDdata = instuff['FragIDdata']
 COMIDdata = instuff['COMIDdata']
+'''
+
+
 
 Segmentdata = SFRc.SFRSegmentsAll()
 Segmentdata.divide_at_confluences(LevelPathdata, FragIDdata, COMIDdata, CELLdata, SFRdata)
@@ -91,7 +91,7 @@ Segmentdata.accumulate_same_levelpathID(LevelPathdata, COMIDdata, FragIDdata, SF
 SFRoutput = SFRc.SFRoutput(SFRdata)
 SFRoutput.write_SFR_tables(Segmentdata)
 SFRoutput.build_SFR_package()
-SFRoutput.build_SFR_shapefile(Segmentdata)
+#SFRoutput.build_SFR_shapefile(Segmentdata)
 
 
 
