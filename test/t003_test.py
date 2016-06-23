0648556db0ac0ef94c630a87800aa8938152d1de
Leaf <aleaf@usgs.gov>
2016-06-23 11:01:26 -0500

    trying to create merge conflict

---
+++ test/t003_test.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('..')
+import os
+import sys
+sys.path.append('..')
+import os
+from GISio import shp2df, get_proj4
+from preproc import lines, NHDdata
+from GISio import shp2df, get_proj4
+from preproc import lines, NHDdata
