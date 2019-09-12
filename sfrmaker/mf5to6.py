fix some bugs related to mf6 and length units

sfrmaker/mf5to6.py
@@ -9,14 +9,14 @@ import sfrmaker
 class mf6sfr:
 
     # convert from ModflowSfr to mf6
-    mf6names = {'reachID': 'rno',
+    mf6names = {'rno': 'rno',
                 'node': 'cellid',
                 'rchlen': 'rlen',
                 'slope': 'rgrd',
                 'strtop': 'rtp',
                 'strthick': 'rbth',
                 'strhc1': 'rhk',
-                'roughch': 'manning',
+                'roughch': 'man',
                 'flow': 'inflow',
                 'pptsw': 'rainfall',
                 'etsw': 'evaporation',
@@ -55,13 +55,14 @@ class mf6sfr:
 
         # dataframes of reach and segment data from ModflowSfr2
         self.rd = pd.DataFrame(ModflowSfr2.reach_data)
+        self.rd.rename(columns={'reachID': 'rno'}, inplace=True)
         self.sd = self._get_segment_dataframe()
 
         # package data
         self._package_data = None
 
         # connection info (doesn't support diversions)
-        self.graph = dict(zip(self.rd.reachID, self.rd.outreach))
+        self.graph = dict(zip(self.rd.rno, self.rd.outreach))
         sd0 = pd.DataFrame(ModflowSfr2.segment_data[0])
         self._graph_r = None # filled by properties
         self.outlets = None
@@ -86,7 +87,7 @@ class mf6sfr:
     def graph_r(self):
         if self._graph_r is None:
             outreaches = np.unique(self.rd.outreach)
-            self._graph_r = {o: self.rd.reachID[self.rd.outreach == o].tolist()
+            self._graph_r = {o: self.rd.rno[self.rd.outreach == o].tolist()
                              for o in outreaches}
             self.outlets = self._graph_r[0]
             del self._graph_r[0]
@@ -196,7 +197,7 @@ class mf6sfr:
 
         # get rno for reach 1s
         segroups = self.rd.groupby('iseg')
-        reach1IDs = segroups.first()['reachID'].to_dict()
+        reach1IDs = segroups.first()['rno'].to_dict()
         sd['rno'] = [reach1IDs[i + 1] for i in sd.index]
         # sd['rainfall'] = np.random.randn(len(sd))
 
@@ -217,7 +218,7 @@ class mf6sfr:
             # iterate through segments in sd (multiple periods)
             for i, r in sd.iterrows():
                 seg, per = int(r.nseg), int(r.per)
-                df = pd.DataFrame(segroups.get_group(seg)[['reachID', 'rchlen', 'ireach']])
+                df = pd.DataFrame(segroups.get_group(seg)[['rno', 'rchlen', 'ireach']])
                 df.rename(columns={'reachID': 'rno'}, inplace=True)
                 df['iseg'] = seg
                 df['lenfrac'] = df.rchlen / df.rchlen.sum()
@@ -259,8 +260,9 @@ class mf6sfr:
 
         # for icalc=0 reaches with no depth specified in per 0
         # set stage from strtop; add to distributed period data
-        strtop = self.rd[['reachID', 'strtop', 'iseg', 'ireach']].copy()
-        strtop.rename(columns={'reachID': 'rno', 'strtop': 'stage'}, inplace=True)
+        strtop = self.rd[['rno', 'strtop', 'iseg', 'ireach']].copy()
+        strtop.rename(columns={#'reachID': 'rno',
+                               'strtop': 'stage'}, inplace=True)
         strtop['icalc'] = [icalc[(s, 0)] for s in self.rd.iseg]
         strtop.index = strtop.rno
         # cull to only include per=0 reaches where icalc=0 and depth wasn't specified

sfrmaker/sfrdata.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('/Users/aleaf/Documents/GitHub/flopy3')
 import os
 import time
+import copy
 import numpy as np
 import pandas as pd
 import rasterio
@@ -59,15 +60,15 @@ class sfrdata:
     """
 
     # conversions to MODFLOW6 variable names
-    mf6names = {'reachID': 'rno',
-                'node': 'cellid',
+    mf6names = {'rno': 'rno',
+                #'node': 'cellid',
                 'rchlen': 'rlen',
                 'width': 'rwid',
                 'slope': 'rgrd',
                 'strtop': 'rtp',
                 'strthick': 'rbth',
                 'strhc1': 'rhk',
-                'roughch': 'manning',
+                'roughch': 'man',
                 'flow': 'inflow',
                 'pptsw': 'rainfall',
                 'etsw': 'evaporation',
@@ -103,7 +104,7 @@ class sfrdata:
               'name': np.object, 'geometry': np.object}
 
     # LENUNI = {"u": 0, "f": 1, "m": 2, "c": 3}
-    len_const = {1: 1.486, 2: 1.0, 3: 100.}
+    len_const = {0: 1.0, 1: 1.486, 2: 1.0, 3: 100.}
     # {"u": 0, "s": 1, "m": 2, "h": 3, "d": 4, "y": 5}
     time_const = {1: 1., 2: 60., 3: 3600., 4: 86400., 5: 31557600.}
 
@@ -118,7 +119,7 @@ class sfrdata:
                  segment_data=None, grid=None, sr=None,
                  model=None,
                  isfr=None,
-                 model_length_units='feet', model_time_units='d',
+                 model_length_units="unspecified", model_time_units='d',
                  enforce_increasing_nsegs=True,
                  package_name=None,
                  **kwargs):
@@ -186,7 +187,9 @@ class sfrdata:
     @property
     def _lenuni(self):
         """MODFLOW length units code"""
-        d = {"feet": 1, "meters": 2}
+        d = {"unspecified": 0,
+             "feet": 1,
+             "meters": 2}
         return d[self.model_length_units]
 
     @property
@@ -423,7 +426,7 @@ class sfrdata:
         self.reach_data['ireach'] = ireach
 
     def set_outreaches(self):
-        """Determine the outreach for each SFR reach (requires a reachID column in reach_data).
+        """Determine the outreach for each SFR reach (requires a rno column in reach_data).
         Uses the segment routing specified for the first stress period to route reaches between segments.
         """
         self.reach_data.sort_values(by=['iseg', 'ireach'], inplace=True)
@@ -448,7 +451,7 @@ class sfrdata:
                     nextrchid = reach1IDs[nextseg] # get reach 1 of next segment
                 else:
                     nextrchid = 0
-            else:  # otherwise, it's the next reachID
+            else:  # otherwise, it's the next rno
                 nextrchid = rno[i + 1]
             outreach.append(nextrchid)
         self.reach_data['outreach'] = outreach
@@ -544,6 +547,7 @@ class sfrdata:
     def create_mf6sfr(self, model=None, unit_conversion=None,
                       stage_filerecord=None,
                       budget_filerecord=None,
+                      flopy_rno_input_is_zero_based=True,
                       **kwargs
                       ):
 
@@ -577,6 +581,7 @@ class sfrdata:
 
         # create an sfrmaker.mf6sfr instance
         from .mf5to6 import mf6sfr
+
         sfr6 = mf6sfr(self.ModflowSfr2)
 
         # package data
@@ -589,11 +594,33 @@ class sfrdata:
             columns = packagedata.drop(['k', 'i', 'j', 'idomain'], axis=1).columns.tolist()
             packagedata['cellid'] = list(zip(packagedata.k,
                                              packagedata.i,
-                                             packagedata.k))
+                                             packagedata.j))
             columns.insert(1, 'cellid')
-        packagedata = packagedata[columns].values.tolist()
+
         connectiondata = [(rno, *sfr6.connections[rno])
-                          for rno in sfr6.packagedata.rno]
+                          for rno in sfr6.packagedata.rno if rno in sfr6.connections]
+        # as of 9/12/2019, flopy.mf6.modflow.ModflowGwfsfr requires zero-based input for rno
+        if flopy_rno_input_is_zero_based and self.ModflowSfr2.reach_data['reachID'].min() == 1:
+            packagedata['rno'] -= 1
+            zero_based_connectiondata = []
+            for item in connectiondata:
+                zb_items = tuple(i-1 if i > 0 else i+1 for i in item)
+                zero_based_connectiondata.append(zb_items)
+            connectiondata = zero_based_connectiondata
+        assert packagedata['rno'].min() == 0
+        assert np.min(list(map(np.min, map(np.abs, connectiondata)))) < 1
+
+        # set cellids to None for unconnected reaches or where idomain == 0
+        # note: as of 9/12/2019, this results in an error during ModflowGwfsfr package construction
+        # with either Nonetype or "NONE"
+        # even though MODFLOW 6 requires it
+        #unconnected = ~packagedata['rno'].isin(np.array(list(sfr6.connections.keys())) - 1).values
+        #inactive = m.dis.idomain.array[packagedata.k.values,
+        #                               packagedata.i.values,
+        #                               packagedata.j.values]
+        #packagedata.loc[unconnected | inactive, 'cellid'] = None
+        packagedata = packagedata[columns].values.tolist()
+
         perioddata = None
         if sfr6.perioddata is not None:
             raise NotImplemented("Support for mf6 perioddata input not implemente yet. "
@@ -860,7 +887,9 @@ class sfrdata:
         # recreate the flopy package object in case it changed
         self.create_ModflowSfr2(model=self.model, **kwargs)
         header_txt = "#SFR package created by SFRmaker v. {}, " \
-                     "via FloPy v. {}".format(sfrmaker.__version__, flopy.__version__)
+                     "via FloPy v. {}\n".format(sfrmaker.__version__, flopy.__version__)
+        header_txt += "#model length units: {}, model time units: {}".format(self.model_length_units,
+                                                                                 self.model_time_units)
         self.ModflowSfr2.heading = header_txt
 
         if version == 'mf2005':

sfrmaker/test/test_sfr_data.py
@@ -1,4 +1,7 @@
+import os
 import pytest
+import numpy as np
+import pandas as pd
 import flopy
 import sfrmaker
 
@@ -21,7 +24,67 @@ def test_const(shellmound_sfrdata, sfrdata):
     assert sfrdata.const == 86400 * 1.486
 
 
-def test_create_mf6sfr(shellmound_sfrdata, shellmound_model):
-    mf6sfr = shellmound_sfrdata.create_mf6sfr(model=shellmound_model)
+@pytest.fixture(scope="function")
+def mf6sfr(shellmound_sfrdata, shellmound_model):
+    return shellmound_sfrdata.create_mf6sfr(model=shellmound_model)
+
+
+@pytest.fixture(scope="function")
+def mf6sfr_outfile(mf6sfr, shellmound_model, outdir):
+    m = shellmound_model
+    mf6sfr.write()
+    outfile = os.path.join(m.model_ws, mf6sfr.filename)
+    return outfile
+
+
+def test_create_mf6sfr(mf6sfr, shellmound_sfrdata, shellmound_model):
     assert isinstance(mf6sfr, flopy.mf6.ModflowGwfsfr)
     assert hasattr(shellmound_model, 'sfr')
+
+    # test that packagedata has same information as sfrmaker.sfrdata
+    packagedata = pd.DataFrame(mf6sfr.packagedata.array)
+    k, i, j = list(zip(*packagedata['cellid'].tolist()))
+    packagedata['k'] = k
+    packagedata['i'] = i
+    packagedata['j'] = j
+    packagedata.drop('cellid', axis=1, inplace=True)
+    for col in packagedata.columns:
+        if packagedata[col].dtype == np.object:
+            packagedata[col] = pd.to_numeric(packagedata[col])
+    reach_data = shellmound_sfrdata.reach_data
+    assert np.array_equal(packagedata['rno'].values + 1, reach_data['rno'].values)
+    packagedata['iseg'] = reach_data['iseg']
+    segment_data = shellmound_sfrdata.segment_data
+    packagedata_groups = packagedata.groupby('iseg').mean()
+    for col in packagedata.columns:
+        if col in shellmound_sfrdata.mf5names:
+            mf2005col = shellmound_sfrdata.mf5names[col]
+            if mf2005col in reach_data.columns:
+                c1 = packagedata[col].values
+                c2 = reach_data[mf2005col].values
+                if col == 'rno':
+                    c1 += 1
+                assert np.allclose(c1, c2, rtol=0.001), \
+                    print('values in packagedata.{} != sfrdata.reach_data.{}'.format(col, mf2005col))
+            elif mf2005col in segment_data.columns:
+                assert np.allclose(packagedata_groups[col].values, segment_data[mf2005col].values, rtol=0.001), \
+                    print('values in packagedata.{} != sfrdata.segment_data.{}'.format(col, mf2005col))
+
+
+def test_flopy_mf6sfr_outfile(mf6sfr, mf6sfr_outfile):
+    assert os.path.exists(mf6sfr_outfile)
+    # check that the written package data matches in memory package data
+    pkdata1 = mf6sfr.packagedata.array.copy()
+    mf6sfr.load()
+    for col in pkdata1.dtype.names:
+        if col == 'cellid':
+            continue
+        elif pkdata1[col].dtype == np.object:
+            c1 = pd.to_numeric(pkdata1[col])
+            c2 = pd.to_numeric(mf6sfr.packagedata.array[col])
+        else:
+            c1 = pkdata1[col]
+            c2 = mf6sfr.packagedata.array[col]
+        assert np.allclose(c1, c2)
+
+