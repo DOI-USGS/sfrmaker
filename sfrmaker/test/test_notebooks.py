5a8239cab5a6860d07ee3968841c20d197a7f200
aleaf <andrew.t.leaf@gmail.com>
2020-02-12 11:28:19 -0600

    added kernel detection to notebook testing

--- sfrmaker/test/test_notebooks.py
+++ sfrmaker/test/test_notebooks.py
@@ -17,18 +17,35 @@ def notebook(request):
     return request.param
 
 
+@pytest.fixture(scope='session')
+def kernel_name():
+    """Pick a Jupyter Notebook kernel from the ones available.
+    """
+    import jupyter_client
+    M = jupyter_client.kernelspec.KernelSpecManager()
+    specs = M.find_kernel_specs()
+
+    # try using the first one of these kernels that is found
+    try_kernel_names = ['test', 'sfrmaker', 'gis']
+    for name in try_kernel_names:
+        if name in specs:
+            return name
+    # otherwise use the first kernel listed in specs
+    return list(specs.keys())[0]
+
+
 # even though test runs locally on Windows 10, and on Travis
 @pytest.mark.xfail(os.environ.get('APPVEYOR') == 'True',
                    reason="jupyter kernel has timeout issue on appveyor for some reason")
-def test_notebook(notebook, outdir):
+def test_notebook(notebook, kernel_name, tmpdir):
     # run autotest on each notebook
     path, fname = os.path.split(notebook)
     cmd = ('jupyter ' + 'nbconvert '
            '--ExecutePreprocessor.timeout=600 '
-           '--ExecutePreprocessor.kernel_name=test '
+           '--ExecutePreprocessor.kernel_name={} '.format(kernel_name) +
            '--to ' + 'notebook '
            '--execute ' + '{} '.format(notebook) +
-           '--output-dir ' + '{} '.format(outdir) +
+           '--output-dir ' + '{} '.format(tmpdir) +
            '--output ' + '{}'.format(fname))
     ival = os.system(cmd)
     assert ival == 0, 'could not run {}'.format(fname)
