43fecf90923aa2969e72d887dab4e604f1a1e780
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-11-12 22:40:30 +0000

    Update docs after building Travis build 176 of aleaf/sfrmaker
    The docs were built from the branch 'develop' against the commit
    2e44c8e6a90eed62f33b353ab2f88b6ed826a6de.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/aleaf/sfrmaker/jobs/437937400.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy --built-docs docs/build/html . --branch-whitelist master develop

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.2.post0.dev0+g45ec491',
+    VERSION: '0.6.2.post1.dev0+g2e44c8e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
