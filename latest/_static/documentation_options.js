fb74bbef92b8ccc61bd2b012a9ec025f375a65d2
aleaf <andrew.t.leaf@gmail.com>
2021-09-15 12:41:17 +0000

    Deploying to gh-pages from  @ 878d571452529a193349bc5336165648e4cdc067 🚀

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.0.post22.dev0+g3caae92',
+    VERSION: '0.8.0.post23.dev0+g878d571',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
