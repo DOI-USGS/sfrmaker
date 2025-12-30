61f89ac01058f1f4e966a174d0c4c36b880683b1
aleaf <andrew.t.leaf@gmail.com>
2025-12-30 13:05:12 +0000

    Deploying to gh-pages from @ DOI-USGS/sfrmaker@70baf6863b5e370e18e295b09770e665b8227e55 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.12.1.post0.dev0+g47043eb',
+    VERSION: '0.13.0.post0.dev0+g70baf68',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
